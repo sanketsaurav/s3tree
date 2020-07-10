4c1877e0ef5c298e71ef6ea884894eec8f575749
Sanket Saurav <sanket@deepsource.io>
2020-07-10 16:48:11 +0530

    Create test-autofix-pr.py

---
+++ test-autofix-pr.py
@@ -0,0 +1,30 @@
+import os
+from sys import (
+    path as p,
+    argv
+)
+import math
+# This should be at the top.
+from __future__ import print_function
+import random as ran
+def foo(this_is_unused, x=[1, 2]):
+    x.append(ran.randint(1, 100))
+    return x
+def bar():
+    print("Hey there, Reviewer!")
+def spam():
+    print("Hope you're paying attention to the tests too!")
+def give_me_an_issue():
+    print(f"okay. Let's fix this unnecessary fstring.")
+def get_hypotenuse(a, b):
+    """Fine. Fix this pythagorean issue as well."""
+    return math.sqrt(a**2 + b**2)
+__all__ = [
+    "got_hypotenuse",  # Shall be deleted
+    # Yup. Delete this
+    "this_too_would_be_deleted", give_me_an_issue, spam,
+    foo, "future", bar,
+    get_hypotenuse, "ughhh",  # With a comment,
+    "budd",
+    "thudd",
+]
